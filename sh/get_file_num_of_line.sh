4543a5be42569b9c9956a0bdcd2bb86ec37e1d49
toyama <toyama@lambda.phys.tohoku.ac.jp>
2021-02-07 22:12:58 +0900

    add sh/get_file_num_of_line.sh

---
+++ sh/get_file_num_of_line.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#echo $#
+if [ $# -ne 1 ];then
+  echo "usage: ${0} {runnum}"
+  exit
+fi
+
+file=${1}
+
+num_of_line=`grep '' ${file}|wc -l`
+
+#another method
+#num_of_line_wc=`wc -l ${file} | awk '{print $1}'`
+
+num_of_line_with_content=`grep . ${file}|wc -l`
+
+echo 'num_of_line num_of_line_with_content'
+echo "${num_of_line} ${num_of_line_with_content}"
